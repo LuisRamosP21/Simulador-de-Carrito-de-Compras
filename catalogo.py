69e42b90ebf55657d6ceea1735707fbbb0a59ab8
Esmeralda Estrada <estradaguarnerosesmeralda@gmail.com>
2026-09-15 22:35:12 -0600

    archivo del catalogo creado

---
+++ catalogo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 21:37:51 2026
+
+@author: marie
+"""
+catalogo = {
+    "p1": {"nombre":"Pan de muerto ", "precio":"30"},
+    "p2": {"nombre":"Dona de chocolate","precio":"20"},
+    "p3": {"nombre":"Dona de azucar", "precio": "15"}
+    }
+
+print("Catalogo cargado")

a2793b805d8f6b83c270fcaca21e73aa9fda6c5b
Esmeralda Estrada <estradaguarnerosesmeralda@gmail.com>
2026-09-16 00:06:00 -0600

    Se agregaron nuevos productos al catalogo

--- catalogo.py
+++ catalogo.py
@@ -5,9 +5,12 @@ Created on Tue Sep 15 21:37:51 2026
 @author: marie
 """
 catalogo = {
-    "p1": {"nombre":"Pan de muerto ", "precio":"30"},
+    "p1": {"nombre":"Pan de muerto ", "precio":"35"},
     "p2": {"nombre":"Dona de chocolate","precio":"20"},
-    "p3": {"nombre":"Dona de azucar", "precio": "15"}
+    "p3": {"nombre":"Dona de azucar", "precio": "15"},
+    "p4": {"nombre": "Concha de chocolate", "precio": "25"},
+    "p5": {"nombre": "Concha de vainilla", "precio": "25"},
+    "p6": {"nombre": ""}
     }
 
 print("Catalogo cargado")
